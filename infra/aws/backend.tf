b84a24f4f2ef9eab9f9e0df3bacf33e4a2ec4894
Arti <Arti@example.com>
2025-04-09 03:50:56 +0530

    updated cicd.yml,backend.tf

--- infra/aws/backend.tf
+++ infra/aws/backend.tf
@@ -1,8 +1,7 @@
-https://2d92-45-115-184-224.ngrok-free.app/ui/dc1/nodes
 terraform {
   backend "consul" {
-    address = "https://2d92-45-115-184-224.ngrok-free.app"
-    path    = "terraform/state"
+    address = "https://2d92-45-115-184-224.ngrok-free.app:8500"  # Correct HTTP API URL
+    path    = "terraform/state"  # Path to store the state in Consul
   }
 }
 
