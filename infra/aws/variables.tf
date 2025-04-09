1f93f4609757b5908d69c3b8fc04392b1410bc94
Arti <Arti@example.com>
2025-04-09 10:25:31 +0530

    updated ci-cd.yml

--- infra/aws/variables.tf
+++ infra/aws/variables.tf
@@ -13,7 +13,7 @@ variable "aws_ami" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.medium"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
